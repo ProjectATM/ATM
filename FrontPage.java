a11b7477a6626ead62527761fc71c326efd701c9
Mayank2204 <mayankgurjar22@gmail.com>
2016-08-05 23:38:30 +0530

    fully changed the first page

--- FrontPage.java
+++ FrontPage.java
@@ -1,44 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hp
- */
-import java.awt.*;
-import static java.awt.Font.BOLD;
-import javax.swing.*;
-import static javax.swing.text.StyleConstants.Bold;
-// static javax.swing.JFrame.EXIT_ON_CLOSE;
-
-public class FrontPage {
-    public static void main(String[] args) {
-        JFrame jf=new JFrame("Welcome To The United Bank");
-        jf.getContentPane().setBackground(Color.ORANGE );
-        jf.setVisible(true);
-        jf.setLayout(null);
-        jf.setSize(500,500);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE );
-        
-        JButton jb1=new JButton("Continue....");
-        jf.add(jb1);
-        jb1.setBounds(250, 400, 100, 30);
-        
-        JLabel jl1=new JLabel("Welcome To United Bank");
-        jl1.setFont(new Font("Arial", Font.BOLD, 30));
-        jf.add(jl1);
-        jl1.setForeground(Color.BLACK);
-        jl1.setBounds(60, 200, 500, 30);
-        //jl1.setText("Welcome To United Bank");
-        
-         JLabel jl2=new JLabel("Press Button To Continue...");
-        jf.add(jl2);
-        jl2.setForeground(Color.BLACK);
-        jl2.setBounds(10, 400, 500, 30);
-        
+import com.sun.prism.paint.Color;
+public class FrontPage extends javax.swing.JFrame {
+    public FrontPage() {
+        initComponents();
+        getContentPane().setBackground(java.awt.Color.ORANGE);
     }
-    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jl1 = new javax.swing.JLabel();
+        Continue = new javax.swing.JButton();
+        jl2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Welcome To United Bank");
+        setBackground(new java.awt.Color(255, 0, 0));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setForeground(java.awt.Color.blue);
+        setName("frame"); // NOI18N
+
+        jl1.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        jl1.setText("Welcome To United Bank");
+
+        Continue.setText("Continue....");
+        Continue.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ContinueActionPerformed(evt);
+            }
+        });
+
+        jl2.setText("Press button to continue...");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jl2, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Continue)
+                .addGap(69, 69, 69))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addComponent(jl1)
+                .addContainerGap(53, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(110, 110, 110)
+                .addComponent(jl1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Continue)
+                    .addComponent(jl2))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void ContinueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ContinueActionPerformed
+
+        new LoginPage().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_ContinueActionPerformed
+
+   
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrontPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrontPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrontPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrontPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrontPage().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Continue;
+    private javax.swing.JLabel jl1;
+    private javax.swing.JLabel jl2;
+    // End of variables declaration//GEN-END:variables
 }
